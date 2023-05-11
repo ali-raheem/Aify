8702d88381a22620f97d07dfca7086bef6449fa6
Ali Raheem <ali.raheem@gmail.com>
2023-05-11 18:53:41 +0100

    Add more starting prompts

--- html/settings.js
+++ html/settings.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var saveButton = document.getElementById("save-settings");
     var defaultButton = document.getElementById("default-settings");
     var defaultActions = [
+        { name: "Reply to this", prompt: "Reply to the following email." },
         { name: "Rewrite Polite", prompt: "Rewrite the following text to be more polite. Reply with only the re-written text and no extra comments." },
         { name: "Rewrite Formal", prompt: "Rewrite the following text to be more formal. Reply with only the re-written text and no extra comments." },
         { name: "Classify", prompt: "Classify the following text in terms of Politeness, Warmth, Formality, Assertiveness, Offensiveness giving a percentage for each category. Reply with only the category and score and no extra text." },
+        { name: "Summerize this", prompt: "Summerize the following email into a bullet point list." },
         { name: "Prompt provided", prompt: " " },
     ];
     var defaultModel = "gpt-3.5-turbo";
