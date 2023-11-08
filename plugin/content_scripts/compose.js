904873274f7f1c0a4d2bc17b36658a89f8e68b0c
Alex Saveliev <lyolik@codeminders.com>
2023-11-08 16:57:19 +0200

    addresses #3
    added ability to replace current selection with the text generated by ChatGPT

--- plugin/content_scripts/compose.js
+++ plugin/content_scripts/compose.js
@@ -1,5 +1,13 @@
 browser.runtime.onMessage.addListener((message) => {
   if (message.command === "getSelectedText") {
     return Promise.resolve(window.getSelection().toString());
+  } else if (message.command === "replaceSelectedText") {
+    const sel = window.getSelection();
+    if (!sel || sel.type !== "Range" || !sel.rangeCount) {
+      return;
+    }
+    const r = sel.getRangeAt(0);
+    r.deleteContents();
+    r.insertNode(document.createTextNode(message.text));
   }
 });
